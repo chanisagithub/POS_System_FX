1810b3ed09cdc8307b5f8a82b480c87ed5cfa395
Chanisa jayawardhana <sadin.chanisa8@gmail.com>
2023-08-13 10:30:16 +0530

    login styles
    CSS Styles

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+    public TextField txtusername;
+    public TextField txtpassword;
+
+    public void btnLogin(ActionEvent actionEvent) {
+        System.out.println(txtusername.getText());
+        System.out.println(txtpassword.getText());
+    }
 }
